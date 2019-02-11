de0ed963ab11132705357cf064bc4a9e008fc719
InJanus <d6vacv@splm.siemens.com>
2019-02-11 11:54:49 -0500

    V1

---
+++ js/script.js
@@ -0,0 +1,37 @@
+function openPage(pageName, elmnt, color) {
+    // Hide all elements with class="tabcontent" by default */
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+  
+    // Remove the background color of all tablinks/buttons
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].style.backgroundColor = "";
+    }
+  
+    // Show the specific tab content
+    document.getElementById(pageName).style.display = "block";
+  
+    // Add the specific color to the button used to open the tab content
+    elmnt.style.backgroundColor = color;
+  }
+  
+  // Get the element with id="defaultOpen" and click on it
+  document.getElementById("defaultOpen").click();
+  mydate = new Date();
+  age = mydate.getFullYear()-1999;
+  if (mydate.getMonth()<5){
+    age = age -1;
+  }else if(mydate().getMonth()==5){
+    if(mydate().getDate()<26){
+      age = age -1;
+    }
+  }
+  
+
+
+
+  document.getElementById('date').innerHTML = ("Age: " + age);
