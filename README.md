4d295cd041a8f3a9bfb1417a8d9c5f5fb9471745
culberbn <culberbn@mail.uc.edu>
2020-03-12 17:45:51 -0400

    draft commit

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # injanus.net
 
 This is the website injanus.net intended for Brian Culberson
+
+List of strengths and skills for professional development
--- scripts.js
+++ scripts.js
@@ -8,4 +8,33 @@ myApp.controller('textCtrl', function textCtrl($scope, $http){
         $scope.coop2 = lines[2];
         $scope.coop3 = lines[3];
     });
-})
+    $http.get('data/projects.json').then(function(data){
+        $scope.photos = data.data;
+    });
+    $http.get('data/strengths.json').then(function(data){
+        $scope.strengths = data.data;
+    });
+    $http.get('data/skills.json').then(function(data){
+        $scope.skills = data.data;
+    });
+    
+    $scope.currentIndex = 0;
+    function getCurrentSlideIndex() {
+        return $scope.currentIndex;
+    }
+    $scope.isCurrentSlideIndex = function isCurrentSlideIndex(index){
+        return $scope.currentIndex === index;
+    }
+
+    $scope.moveLeft = function moveLeft(){
+        if(getCurrentSlideIndex() > 0){
+            $scope.currentIndex = $scope.currentIndex - 1;
+        }
+    }
+
+    $scope.moveRight = function moveRight(){
+        if(getCurrentSlideIndex() < $scope.photos.length-1){
+            $scope.currentIndex = $scope.currentIndex + 1;
+        }
+    }
+});
