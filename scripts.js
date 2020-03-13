0729ff94332781df8aa5a3993993eefba0205b5a
culberbn <culberbn@mail.uc.edu>
2020-03-13 01:47:47 -0400

    closer commit

--- scripts.js
+++ scripts.js
@@ -3,10 +3,11 @@ var myApp = angular.module('myApp', []);
 myApp.controller('textCtrl', function textCtrl($scope, $http){
     $http.get('data/about.txt').then(function(data){
         var lines = data.data.split('\n');
-        $scope.about = lines[0];
-        $scope.coop1 = lines[1];
-        $scope.coop2 = lines[2];
-        $scope.coop3 = lines[3];
+        $scope.about1 = lines[0];
+        $scope.about2 = lines[1];
+        $scope.coop1 = lines[2];
+        $scope.coop2 = lines[3];
+        $scope.coop3 = lines[4];
     });
     $http.get('data/projects.json').then(function(data){
         $scope.photos = data.data;
@@ -17,7 +18,7 @@ myApp.controller('textCtrl', function textCtrl($scope, $http){
     $http.get('data/skills.json').then(function(data){
         $scope.skills = data.data;
     });
-    
+
     $scope.currentIndex = 0;
     function getCurrentSlideIndex() {
         return $scope.currentIndex;
